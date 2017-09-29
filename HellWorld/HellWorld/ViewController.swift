66246e383d3ff84a2110ba85a78f08b08027b650
stories2 <stories2@naver.com>
2017-09-30 02:14:54 +0900

    HellWorld module : make new project

---
+++ HellWorld/HellWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HellWorld
+//
+//  Created by 김현우 on 2017. 9. 30..
+//  Copyright © 2017년 김현우. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
