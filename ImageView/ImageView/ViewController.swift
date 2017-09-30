0b9e3990f86a0fe828810bcb83e9906e9770ecc7
stories2 <stories2@naver.com>
2017-09-30 19:57:27 +0900

    ImageView module : setup layout

--- ImageView/ImageView/ViewController.swift
+++ ImageView/ImageView/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var switchImage: UISwitch!
+    @IBOutlet weak var btnChangeScale: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func SwitchImage(_ sender: UISwitch) {
+    }
+    @IBAction func BtnChangeScale(_ sender: UIButton) {
+    }
+    
 }
 
