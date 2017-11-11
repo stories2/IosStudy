71789deef640554360d7380ee0f4c3ffc14e52fe
stories2 <stories2@naver.com>
2017-11-12 00:07:12 +0900

    multi touch app project added

---
+++ MultiTouch/MultiTouch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MultiTouch
+//
+//  Created by 김현우 on 2017. 11. 12..
+//  Copyright © 2017년 김현우. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
