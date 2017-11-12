aa603542086acecbc7e7d0cc4d505a096274e5e6
stories2 <stories2@naver.com>
2017-11-12 17:51:52 +0900

    moving sketch view

--- SketchApp/SketchApp/ViewController.swift
+++ SketchApp/SketchApp/ViewController.swift
@@ -9,12 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var sketchImage: UIImageView!
+    @IBOutlet weak var sketchImageView: UIImageView!
     var lastTouchPoint: CGPoint!
     var lineWidth: CGFloat = 2.0
     var lineColor = UIColor.gray.cgColor
     var lastImage = [UIImage]()
     var sketchImageStackPoint: Int = 0
+    var lastHandPoint: CGPoint!, lastSketchViewPoint: CGPoint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,28 +26,33 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func moveSketchImageView(_ transformX: CGFloat, transformY: CGFloat) {
+        sketchImageView.transform.tx = transformX
+        sketchImageView.transform.ty = transformY
+    }
 
     @IBAction func btnOnBackClick(_ sender: UIButton) {
         if(sketchImageStackPoint > 0) {
             sketchImageStackPoint -= 1;
-            sketchImage.image = lastImage[sketchImageStackPoint]
+            sketchImageView.image = lastImage[sketchImageStackPoint]
         }
         else {
             sketchImageStackPoint = 0;
-            sketchImage.image = nil
+            sketchImageView.image = nil
         }
     }
     @IBAction func btnOnForwardClick(_ sender: UIButton) {
         if(sketchImageStackPoint < lastImage.count - 1) {
             sketchImageStackPoint += 1
-            sketchImage.image = lastImage[sketchImageStackPoint]
+            sketchImageView.image = lastImage[sketchImageStackPoint]
         }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         let touch = touches.first! as UITouch
         if(touch.type == UITouchType.stylus) {
-            lastTouchPoint = touch.location(in: sketchImage)
+            lastTouchPoint = touch.location(in: sketchImageView)
             let lastImageStackSize = lastImage.count
             if(sketchImageStackPoint < lastImageStackSize - 1) {
                 print("stack point: ", sketchImageStackPoint, " last stack size: ", lastImageStackSize)
@@ -56,38 +62,62 @@ class ViewController: UIViewController {
                 }
             }
         }
+        else if(touch.type == UITouchType.direct) {
+            lastHandPoint = touch.location(in: sketchImageView)
+            let sketchViewX:CGFloat = sketchImageView.transform.tx
+            let sketchViewY:CGFloat = sketchImageView.transform.ty
+            var sketchViewPoint:CGPoint = CGPoint(x: sketchViewX, y: sketchViewY)
+            lastSketchViewPoint = sketchViewPoint
+        }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         let touch = touches.first! as UITouch
         if(touch.type == UITouchType.stylus) {
-            let currentTouchPoint = touch.location(in: sketchImage)
+            let currentTouchPoint = touch.location(in: sketchImageView)
             let pencilLineWidth = touch.force * lineWidth
             
-            UIGraphicsBeginImageContext(sketchImage.frame.size)
+            UIGraphicsBeginImageContext(sketchImageView.frame.size)
             let graphicsContext = UIGraphicsGetCurrentContext()
             graphicsContext?.setStrokeColor(lineColor)
             graphicsContext?.setLineCap(CGLineCap.round)
             graphicsContext?.setLineWidth(pencilLineWidth)
             
-            sketchImage.image?.draw(in: CGRect(x: 0, y: 0, width: sketchImage.frame.size.width, height: sketchImage.frame.size.height))
+            sketchImageView.image?.draw(in: CGRect(x: 0, y: 0, width: sketchImageView.frame.size.width, height: sketchImageView.frame.size.height))
             
             graphicsContext?.move(to: lastTouchPoint)
             graphicsContext?.addLine(to: currentTouchPoint)
             graphicsContext?.strokePath()
             
-            sketchImage.image = UIGraphicsGetImageFromCurrentImageContext()
+            sketchImageView.image = UIGraphicsGetImageFromCurrentImageContext()
             UIGraphicsEndImageContext()
             
             lastTouchPoint = currentTouchPoint
         }
+        else if(touch.type == UITouchType.direct) {
+            let currentTouchPoint = touch.location(in: sketchImageView)
+            let gapWidth = currentTouchPoint.x - lastHandPoint.x
+            let gapHeight = currentTouchPoint.y - lastHandPoint.y
+            
+            moveSketchImageView(lastSketchViewPoint.x + gapWidth, transformY: lastSketchViewPoint.y + gapHeight)
+        }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first! as UITouch
         
-        if(sketchImage.image != nil) {
-            lastImage.append(sketchImage.image!)
+        if(sketchImageView.image != nil) {
+            lastImage.append(sketchImageView.image!)
             sketchImageStackPoint += 1;
+        }
+        if(touch.type == UITouchType.stylus) {
+        }
+        else if(touch.type == UITouchType.direct) {
+            let currentTouchPoint = touch.location(in: sketchImageView)
+            let gapWidth = currentTouchPoint.x - lastHandPoint.x
+            let gapHeight = currentTouchPoint.y - lastHandPoint.y
+            
+            moveSketchImageView(lastSketchViewPoint.x + gapWidth, transformY: lastSketchViewPoint.y + gapHeight)
         }
     }
 }
