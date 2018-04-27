b000c039c14f75a25e52ff6126c72e5d63f1b90b
Kim HyunWoo <stories2@naver.com>
2018-04-27 19:17:51 +0900

    HelloWorld : make simple music player

--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -7,15 +7,64 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+    
+    var player: AVAudioPlayer!
+    var timer: Timer!
     
     @IBOutlet var playPauseBtn: UIButton!
     @IBOutlet var playTimeLabel: UILabel!
     @IBOutlet var playTimeSlider: UISlider!
+    
+    func initializePlayer() {
+        guard let soundAsset: NSDataAsset = NSDataAsset(name: "ncs-spectre") else {
+            print("cannot load mp3 file")
+            return
+        }
+        
+        do {
+            try self.player = AVAudioPlayer(data: soundAsset.data)
+            self.player.delegate = self
+        } catch let error as NSError {
+            print("initialize failed")
+            print("error code: \(error.code), message: \(error.localizedDescription)")
+        }
+        
+        self.playTimeSlider.maximumValue = Float(self.player.duration)
+        self.playTimeSlider.minimumValue = 0
+        self.playTimeSlider.value = Float(self.player.currentTime)
+    }
+    
+    func updatePlayTimeLabel(time: TimeInterval) {
+        let min: Int = Int(time / 60)
+        let sec: Int = Int(time.truncatingRemainder(dividingBy: 60))
+        let milisec: Int = Int(time.truncatingRemainder(dividingBy: 1) * 100)
+        
+        let timeText: String = String(format: "%02ld:%02ld:%02ld", min, sec, milisec)
+        self.playTimeLabel.text = timeText
+    }
+    
+    func makeAndStartTimer() {
+        self.timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true, block: { [unowned self] (timer: Timer) in
+            if self.playTimeSlider.isTracking{
+                return
+            }
+            
+            self.updatePlayTimeLabel(time: self.player.currentTime)
+            self.playTimeSlider.value = Float(self.player.currentTime)
+        })
+    }
+    
+    func invalidateTimer() {
+        self.timer.invalidate()
+        self.timer = nil
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        initializePlayer()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -24,6 +73,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func touchUpPlayPauseBtn(_ sender: UIButton) {
+        print("touch up play pause btn")
+        sender.isSelected = !sender.isSelected
+        
+        if sender.isSelected {
+            self.player?.play()
+        }
+        else {
+            self.player?.pause()
+        }
+        
+        if sender.isSelected {
+            self.makeAndStartTimer()
+        }
+        else {
+            self.invalidateTimer()
+        }
+    }
+    
+    @IBAction func sliderValChanged(_ sender: UISlider) {
+        print("slider value changed")
+        if sender.isTracking {
+            return
+        }
+        self.player.currentTime = TimeInterval(sender.value)
+    }
 }
 
